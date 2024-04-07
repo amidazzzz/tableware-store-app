ead616273de404e1c05042e8b06f6902061f9b0f
amidazzzz <cain.wells@mail.ru>
2024-04-07 11:39:06 +0300

    Fixed version problem

--- src/main/java/org/amida/tablewarestoreapp/controller/LoginController.java
+++ src/main/java/org/amida/tablewarestoreapp/controller/LoginController.java
@@ -3,6 +3,7 @@ package org.amida.tablewarestoreapp.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,18 +26,26 @@ public class LoginController {
     @FXML
     private void loginButtonClicked(ActionEvent event) {
         try {
+            // Загрузить FXML файл
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/amida/tablewarestoreapp/auth/logged-in.fxml"));
+
+            // Загрузить корневой элемент из FXML файла
             Parent root = loader.load();
 
-            Stage stage = (Stage) loginButton.getScene().getWindow();
+            // Получить текущее окно (Stage)
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
+            // Создать новую сцену с загруженным корневым элементом
             Scene scene = new Scene(root);
 
+            // Установить новую сцену на текущее окно
             stage.setScene(scene);
 
+            // Показать окно
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
